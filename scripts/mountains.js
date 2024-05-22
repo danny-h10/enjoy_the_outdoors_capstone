3a1275b0b35259bee8f385dfc3bae372230949c5
Daniel Hernandez <dh893177@gmail.com>
2024-05-22 13:43:29 -0500

    finished mountains card

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -3,14 +3,79 @@
 
 window.onload = () => {
 
-    /*
-    You can remove the following console.log() lines.
-    They are here to verify that we have access to the data
-    The data script files are located in the scripts/data directory
-*/
+    initMountCards();
 
-    //log the mountainsArray to the console (scripts/data/mountainData.js)
-    console.log(mountainsArray)
+    let mountDropdown = document.querySelector("#mountainSelect")
 
+    mountDropdown.addEventListener("change", displayMountCard)
 
+}
+
+function displayMountCard(event) {
+    let mountDiv = document.querySelector("#mountains")
+
+    mountDiv.innerHTML = ""
+
+    let mountain = mountainsArray.find((mountain) => mountain.name === event.target.value)
+
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card", "w-25");
+
+    let cardImage = document.createElement("img");
+    cardImage.classList.add("card-img-top", "card-img-fit");
+
+    cardImage.src = mountain.img
+    cardImage.alt = mountain.name
+
+    cardDiv.appendChild(cardImage);
+
+    let cardBody = document.createElement("div")
+    cardDiv.classList.add("card-body")
+
+    let cardTitle = document.createElement("h5")
+    cardTitle.innerHTML = mountain.name
+
+    cardBody.appendChild(cardTitle)
+
+    let cardText = document.createElement("p")
+    cardTitle.classList.add("card-text")
+
+    cardText.innerHTML = mountain.desc
+
+    cardBody.appendChild(cardText)
+    cardDiv.appendChild(cardBody)
+    mountDiv.appendChild(cardDiv)
+
+}
+
+function initMountCards() {
+    let mountDropdown = document.querySelector("#mountainSelect");
+
+    //create the element for the default option
+    let defaultOption = document.createElement("option");
+
+    //this is whate we get back in JS when we ask for the selected value
+    defaultOption.value = "";
+
+    //this is what the user actually selects from the dropdown
+    defaultOption.textContent = "--Select a Mountain--";
+
+    //add the option we created to the dropdown
+    mountDropdown.appendChild(defaultOption);
+
+    //write a loop to work with each individual category and build an option for it
+    mountainsArray.forEach((mountain) => {
+
+        //create the new option for the category we are on in the loop
+        let newOption = document.createElement("option");
+
+        //set the value for the option
+        newOption.value = mountain.name;
+
+        //set the textContent that the user will see when choosing a category
+        newOption.textContent = mountain.name;
+
+        mountDropdown.appendChild(newOption);
+
+    })
 }
